0f810bd9c88357aad04ef39ee91eafd615f66bc6
Vadim Shalamov <reetvvy@gmail.com>
2024-03-11 00:06:40 +0200

    projekt start

---
+++ src/test/java/projektAspiration/bookingService/BookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package projektAspiration.bookingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
